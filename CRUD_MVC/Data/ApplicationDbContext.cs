425d2e226ca7511de29b8df359acd484685ce378
jos-gabriel <jos.gabrie@gmail.com>
2023-08-14 03:25:09 -0600

    feat(Moldel-Usuario): Conectar y crear BD Se agregan NuGet, string de conexión y se inicia servicio

---
+++ CRUD_MVC/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using CRUD_MVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUD_MVC.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+            
+        }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
---
+++ CRUD_MVC/Models/Usuario.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CRUD_MVC.Models
+{
+    public class Usuario
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string? Nombre { get; set; }
+
+        public DateTime? Fecha { get; set; }
+
+        public string? Clave { get; set; }
+
+    }
+}
